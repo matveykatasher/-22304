cf82e77a48b97049e4404aa2ab3c1285a7bb4307
Matveykatasher <qtar22405@gmail.com>
2023-02-17 21:23:02 +0300

    Update zad1.c

--- задание 1/zad1.c
+++ задание 1/zad1.c
@@ -1,46 +1,56 @@
 #include <stdio.h>
 #include <math.h>
 #include <ctype.h>
+#include <stdlib.h>
 
 int main()
 {
-	int a,b,c,d;
-	scanf("%d",&a);
-	scanf("%d",&b);
-	scanf("%d",&c);
-	if (!(isdigit((int)a))||!(isdigit((int)b))||!(isdigit((int)c)))
+	int tr=0;
+	int a,b,c,d,n,x,y;
+	while (tr==0)
 	{
-		if (a==0)
+		scanf("%d",&a);
+		scanf("%d",&b);
+		scanf("%d",&c);
+		if (!(isdigit((int)a))||!(isdigit((int)b))||!(isdigit((int)c)))
 		{
-			if (b==0)
+			if (a==0)
 			{
-				if (c==0)
+				if (b==0)
 				{
-					printf("all results");
+					if (c==0)
+					{
+						printf("all results");
+					}
+					else{printf("error");}
 				}
-				else{printf("error");}
+				else
+				{
+					int x=-1*c/b;
+				}
 			}
 			else
 			{
-				int x=-1*c/b;
+				d=pow(b,2)-4*a*c;
+				if (d>=0)
+				{
+					x=(-1*b-sqrt(d))/(2*a);
+					y=(-1*b+sqrt(d))/(2*a);
+					printf("%d",x);
+					printf(" ");
+					printf("%d",y);
+				}
+				else{printf("error");}
 			}
 		}
-		else
+		else{printf("error");}
+		printf_s("%d","OK(0/1):");
+		scanf("%d",&n);
+
+		if (n==1)
 		{
-			d=pow(b,2)-4*a*c;
-			if (d>=0)
-			{
-				int x=(-1*b-sqrt(d))/(2*a);
-				int y=(-1*b+sqrt(d))/(2*a);
-				printf("%d",x);
-				printf(" ");
-				printf("%d",y);
-			}
-			else{printf("error");}
+			tr=1;
 		}
 	}
-	else{printf("error");}
-	int N;
-	scanf("%d",N);
 	return 0;
 }
